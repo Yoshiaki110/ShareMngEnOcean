216183722ca3240230d7ce27aea64f6b83dc1651
Yoshiaki Ito <yoshiaki110@gmail.com>
2020-09-20 21:05:07 +0900

    enocean

--- enocean.js
+++ enocean.js
@@ -1,11 +1,54 @@
+'use strict';
+
+const net = require("net");
 const request = require('request');
 
-var options = {
-  uri: "http://172.28.31.48:5000/",
-  headers: {
-    "Content-type": "application/json",
-  },
-  json: { "device":"台車Ｂ","status":"move" }
-};
-request.post(options, function(error, response, body) {
+const uri = "http://192.168.0.125:5000/";
+const host = "127.0.0.1";
+const port = 8000;
+
+function send(device, status) {
+  console.log('device', device);
+  console.log('status', status);
+  var options = {
+    uri: uri,
+    headers: {
+      "Content-type": "application/json",
+    },
+    json: { "device": device, "status": status }
+  };
+  request.post(options, function(error, response, body) {
+  });
+}
+
+
+var client = new net.Socket();
+
+client.connect(port, host, function() {
+  console.log("connect to: " + host + ":" + port);
 });
+
+client.on("data", function(data) {
+  var s = data.toString();
+
+  s = s.replace(/\\n/g, "\\n")
+  .replace(/\\'/g, "\\'")
+  .replace(/\\"/g, '\\"')
+  .replace(/\\&/g, "\\&")
+  .replace(/\\r/g, "\\r")
+  .replace(/\\t/g, "\\t")
+  .replace(/\\b/g, "\\b")
+  .replace(/\\f/g, "\\f");
+   s = s.replace(/[\u0000-\u0019]+/g,"");
+   var obj = JSON.parse(s);
+
+  let id = obj.dataTelegram.deviceId;
+  //console.log(obj.dataTelegram.functions);
+  //console.log(obj.dataTelegram.functions.find(item => item.key === 'CO'));
+  send(id, 'move');
+});
+
+client.on("close", function() {
+  console.log("disconnect from: " + host + ":" + port);
+})
+
